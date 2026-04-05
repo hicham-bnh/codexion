ae5be7d6fb7a3ece3acfea6648266cbdd69ce247
hicham-bnh <benhabibmohammedhicham@gmail.com>
2026-04-05 14:39:02 +0200

    function for creation of threads added

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: mobenhab <mobenhab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 10:29:33 by mobenhab          #+#    #+#             */
-/*   Updated: 2026/04/04 16:56:17 by mobenhab         ###   ########.fr       */
+/*   Updated: 2026/04/05 12:17:12 by mobenhab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,37 +18,43 @@ void* compil(void* arg) {
     return NULL;
 }
 
-int	check_parsing(t_stock *test)
+pthread_t *create_threads(t_stock *stock)
 {
-	if  (test->number_of_coders < 1)
+    pthread_t *threads = malloc(sizeof(pthread_t) * stock->number_of_coders);
+    return (threads);
+}
+
+int	check_parsing(t_stock *stock)
+{
+	if  (stock->number_of_coders < 1)
     	return (1);
-	else if	(test->time_to_burnout < 1)
+	else if	(stock->time_to_burnout < 1)
 	    return (1);
-    else if	(test->time_to_compile < 1)
+    else if	(stock->time_to_compile < 1)
     	return (1);
-    else if	(test->time_to_debug < 1)
+    else if	(stock->time_to_debug < 1)
         return (1);
-    else if	(test->time_to_refactor < 1)
+    else if	(stock->time_to_refactor < 1)
         return (1);
-    else if	(test->number_of_compiles_required < 1)
+    else if	(stock->number_of_compiles_required < 1)
         return (1);
-    else if	(test->dongle_cooldown < 1)
+    else if	(stock->dongle_cooldown < 1)
         return (1);
-    else if	(strcmp(test->scheduler, "fifo") != 0 && strcmp(test->scheduler, "edf") != 0)
+    else if	(strcmp(stock->scheduler, "fifo") != 0 && strcmp(stock->scheduler, "edf") != 0)
         return (1);
     return (0);
 }
 
-void	parsing_input(t_stock *test, char **str)
+void	parsing_input(t_stock *stock, char **str)
 {
-    test->number_of_coders = atoi(str[1]);
-    test->time_to_burnout = atoi(str[2]);
-    test->time_to_compile = atoi(str[3]);
-    test->time_to_debug = atoi(str[4]);
-    test->time_to_refactor = atoi(str[5]);
-    test->number_of_compiles_required = atoi(str[6]);
-    test->dongle_cooldown = atoi(str[7]);
-    test->scheduler = str[8];
+    stock->number_of_coders = atoi(str[1]);
+    stock->time_to_burnout = atoi(str[2]);
+    stock->time_to_compile = atoi(str[3]);
+    stock->time_to_debug = atoi(str[4]);
+    stock->time_to_refactor = atoi(str[5]);
+    stock->number_of_compiles_required = atoi(str[6]);
+    stock->dongle_cooldown = atoi(str[7]);
+    stock->scheduler = str[8];
 }
 
 int main(int ac, char **av)
@@ -58,18 +64,15 @@ int main(int ac, char **av)
         printf("ERROR");
         return (0);
     }
-    t_stock test;
-    parsing_input(&test, av);
-    if (check_parsing(&test))
+    t_stock stock;
+    parsing_input(&stock, av);
+    if (check_parsing(&stock))
     {
         printf("ERROR");
         return (0);
     }
-    pthread_t *threads = malloc(sizeof(pthread_t) * test.number_of_coders);
-    int i = 0;
-    while (threads[i])
-        i++;
-    printf("%d\n", i);
+    pthread_t *threads = create_threads(&stock);
+    free(threads);
 }
 
 // valgrind --tools=helgrind ou --tools=drd
