cadd781c43ac4550f9d8e83240d5f27a115062ba
hicham-bnh <benhabibmohammedhicham@gmail.com>
2026-04-06 18:43:05 +0200

    create_threads manage the creation of threads and threads join

--- threads.c
+++ threads.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/05 20:33:27 by mobenhab          #+#    #+#             */
-/*   Updated: 2026/04/06 18:29:22 by marvin           ###   ########.fr       */
+/*   Updated: 2026/04/06 18:41:43 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,18 @@ pthread_mutex_t *create_mutex(t_pars *stock)
 pthread_t       *create_threads(t_pars *stock)
 {
 	pthread_t	*threads;
-
+        int     i;
+        
+        i = 0;
 	threads = malloc(sizeof(pthread_t) * stock->number_of_coders);
 	if (!threads)
 		return (NULL);
+        while (i < stock->number_of_coders)
+        {
+                if (pthread_create(&threads[i], NULL, NULL, NULL))
+                        return (1);
+                if (pthread_join(threads[i], NULL))
+                        return (1);
+        }
 	return (threads);
 }
