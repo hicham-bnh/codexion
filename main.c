c74ded66c6c450d2f72afb2703024788eacefe2d
hicham-bnh <benhabibmohammedhicham@gmail.com>
2026-04-06 17:15:33 +0200

    test

--- main.c
+++ main.c
@@ -3,31 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mobenhab <mobenhab@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 10:16:49 by mobenhab          #+#    #+#             */
-/*   Updated: 2026/04/05 21:22:38 by mobenhab         ###   ########.fr       */
+/*   Updated: 2026/04/06 17:15:22 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-int	main(int ac, char **av)
-{
-	t_pars		stock;
-	pthread_t	*threads;
+// int	main(int ac, char **av)
+// {
+// 	t_pars		stock;
+// 	pthread_t	*threads;
 
-	if (ac != 9)
-	{
-		printf("ERROR");
-		return (0);
-	}
-	parsing_input(&stock, av);
-	if (check_parsing(&stock))
-	{
-		printf("ERROR");
-		return (0);
-	}
-	threads = create_threads(&stock);
-	free(threads);
-}
+// 	if (ac != 9)
+// 	{
+// 		printf("ERROR");
+// 		return (0);
+// 	}
+// 	parsing_input(&stock, av);
+// 	if (check_parsing(&stock))
+// 	{
+// 		printf("ERROR");
+// 		return (0);
+// 	}
+// 	threads = create_threads(&stock);
+// 	free(threads);
+// }
